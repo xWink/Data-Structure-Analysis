b0b57f15ff56b5a9ff08dec5c30022658799105c
Shawn Kaplan <skaplan@uoguelph.ca>
2019-09-26 16:50:46 -0400

    added list c and h files

---
+++ ds_list.c
@@ -0,0 +1,11 @@
+#include "ds_memory.h"
+
+void ds_create_list();
+int ds_init_list();
+int ds_replace(int value, long index);
+int ds_insert(int value, long index);
+int ds_delete(long index);
+int ds_swap(long index1, long index2);
+long ds_find(int target);
+int ds_read_elements(char *filename);
+int ds_finish_list();
---
+++ ds_list.h
@@ -0,0 +1,14 @@
+struct ds_list_item_struct {
+  int item;
+  long next;
+};
+
+void ds_create_list();
+int ds_init_list();
+int ds_replace(int value, long index);
+int ds_insert(int value, long index);
+int ds_delete(long index);
+int ds_swap(long index1, long index2);
+long ds_find(int target);
+int ds_read_elements(char *filename);
+int ds_finish_list();
